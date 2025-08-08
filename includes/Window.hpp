90301afccc82a22fc8f757e73ce62b1f20726f2d
ThisIsNew145 <Feranmieoke@proton.me>
2025-08-08 20:32:36 +0100

    My fifth commit, I did basic sprites (man sfml does a lot of heavy lifting) thanks for reading, I hope I added value to your day who ever is reading (probably nobody) anyways have a good rest of your day

--- includes/Window.hpp
+++ includes/Window.hpp
@@ -1,12 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
 #include <SFML/Window/Window.hpp>
 
 typedef struct{
   sf::RenderWindow app;
 } Window;
 
-extern sf::RectangleShape rect;
+extern sf::Texture tex;
+extern sf::Sprite sprite;
 
 void CreateWindow(Window* window, int width, int height, std::string& title);
 void Loop(Window* window, sf::Event event);
--- src/Window.cpp
+++ src/Window.cpp
@@ -1,11 +1,15 @@
 #include "../includes/Window.hpp"
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/VideoMode.hpp>
+#include <cstdio>
 
-sf::RectangleShape rect;
+sf::Texture tex;
+sf::Sprite sprite;
 
 void CreateWindow(Window* window, int width, int height, std::string& title){
   window->app.create(sf::VideoMode(width, height), title.c_str());
@@ -27,17 +31,17 @@ void Loop(Window* window, sf::Event event){
 
 void Input(Window *window){
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-    rect.move(0, -1);
+    sprite.move(0, -1);
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-    rect.move(0, +1);
+    sprite.move(0, +1);
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-    rect.move(-1, 0);
+    sprite.move(-1, 0);
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-    rect.move(+1, 0);
+    sprite.move(+1, 0);
 }
 
 void Draw(Window *window){
-  rect.setSize(sf::Vector2(100.f, 100.0f));
-  rect.setFillColor(sf::Color(100, 100, 100, 255));
-  window->app.draw(rect);
+  if (!tex.loadFromFile("assets/Unnamed pokie ball.png")) { printf("The texture could not load"); exit(0);}
+  sprite.setTexture(tex);
+  window->app.draw(sprite);
 }
